1989d39406f6be1d1254306bbdd55191f329521f
satorumasai <162997990+satorumasai@users.noreply.github.com>
2025-04-01 00:44:11 +0900

    Add files via upload

--- Trade2.py
+++ Trade2.py
@@ -2,21 +2,30 @@
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": 1,
+   "execution_count": 7,
    "id": "bd7e32b2-9dcb-413b-9361-3f4fa0f04c90",
    "metadata": {},
    "outputs": [
     {
-     "ename": "ObsoleteAttributeException",
-     "evalue": "app.run_server has been replaced by app.run",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[1;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[1;31mObsoleteAttributeException\u001b[0m                Traceback (most recent call last)",
-      "Cell \u001b[1;32mIn[1], line 63\u001b[0m\n\u001b[0;32m     61\u001b[0m in_jupyter \u001b[38;5;241m=\u001b[39m \u001b[38;5;124m\"\u001b[39m\u001b[38;5;124mJPY_PARENT_PID\u001b[39m\u001b[38;5;124m\"\u001b[39m \u001b[38;5;129;01min\u001b[39;00m os\u001b[38;5;241m.\u001b[39menviron \u001b[38;5;129;01mor\u001b[39;00m \u001b[38;5;124m\"\u001b[39m\u001b[38;5;124mipykernel_launcher\u001b[39m\u001b[38;5;124m\"\u001b[39m \u001b[38;5;129;01min\u001b[39;00m os\u001b[38;5;241m.\u001b[39msys\u001b[38;5;241m.\u001b[39margv[\u001b[38;5;241m0\u001b[39m]\n\u001b[0;32m     62\u001b[0m \u001b[38;5;28;01mif\u001b[39;00m in_jupyter:\n\u001b[1;32m---> 63\u001b[0m     \u001b[43mapp\u001b[49m\u001b[38;5;241;43m.\u001b[39;49m\u001b[43mrun_server\u001b[49m(debug\u001b[38;5;241m=\u001b[39m\u001b[38;5;28;01mTrue\u001b[39;00m, port\u001b[38;5;241m=\u001b[39m\u001b[38;5;241m8050\u001b[39m)  \u001b[38;5;66;03m# Jupyter用\u001b[39;00m\n\u001b[0;32m     64\u001b[0m \u001b[38;5;28;01melse\u001b[39;00m:\n\u001b[0;32m     65\u001b[0m     port \u001b[38;5;241m=\u001b[39m \u001b[38;5;28mint\u001b[39m(os\u001b[38;5;241m.\u001b[39menviron\u001b[38;5;241m.\u001b[39mget(\u001b[38;5;124m\"\u001b[39m\u001b[38;5;124mPORT\u001b[39m\u001b[38;5;124m\"\u001b[39m, \u001b[38;5;241m10000\u001b[39m))\n",
-      "File \u001b[1;32m~\\anaconda3\\Lib\\site-packages\\dash\\_obsolete.py:22\u001b[0m, in \u001b[0;36mObsoleteChecker.__getattr__\u001b[1;34m(self, name)\u001b[0m\n\u001b[0;32m     20\u001b[0m \u001b[38;5;28;01mif\u001b[39;00m name \u001b[38;5;129;01min\u001b[39;00m \u001b[38;5;28mself\u001b[39m\u001b[38;5;241m.\u001b[39m_obsolete_attributes:\n\u001b[0;32m     21\u001b[0m     err \u001b[38;5;241m=\u001b[39m \u001b[38;5;28mself\u001b[39m\u001b[38;5;241m.\u001b[39m_obsolete_attributes[name]\n\u001b[1;32m---> 22\u001b[0m     \u001b[38;5;28;01mraise\u001b[39;00m err\u001b[38;5;241m.\u001b[39mexc(err\u001b[38;5;241m.\u001b[39mmessage)\n\u001b[0;32m     23\u001b[0m \u001b[38;5;28;01mreturn\u001b[39;00m \u001b[38;5;28mgetattr\u001b[39m(\u001b[38;5;28mself\u001b[39m\u001b[38;5;241m.\u001b[39m\u001b[38;5;18m__dict__\u001b[39m, name)\n",
-      "\u001b[1;31mObsoleteAttributeException\u001b[0m: app.run_server has been replaced by app.run"
-     ]
+     "data": {
+      "text/html": [
+       "\n",
+       "        <iframe\n",
+       "            width=\"100%\"\n",
+       "            height=\"650\"\n",
+       "            src=\"http://127.0.0.1:8050/\"\n",
+       "            frameborder=\"0\"\n",
+       "            allowfullscreen\n",
+       "            \n",
+       "        ></iframe>\n",
+       "        "
+      ],
+      "text/plain": [
+       "<IPython.lib.display.IFrame at 0x2809af063c0>"
+      ]
+     },
+     "metadata": {},
+     "output_type": "display_data"
     }
    ],
    "source": [
@@ -27,14 +36,12 @@
     "import pandas as pd\n",
     "import os\n",
     "\n",
-    "# データ読み込み\n",
     "df = pd.read_csv(\"2024BL.csv\")\n",
     "\n",
-    "# Dashアプリ初期化\n",
     "app = dash.Dash(__name__)\n",
-    "server = app.server\n",
+    "app.title = \"貿易統計ダッシュボード\"\n",
+    "server = app.server  # for Render\n",
     "\n",
-    "# Layout定義\n",
     "app.layout = html.Div([\n",
     "    html.H1(\"ドロップダウンで国を選択\"),\n",
     "    dcc.Dropdown(\n",
@@ -45,7 +52,6 @@
     "    dcc.Graph(id='bar-graph')\n",
     "])\n",
     "\n",
-    "# Callback定義\n",
     "@app.callback(\n",
     "    Output('bar-graph', 'figure'),\n",
     "    [Input('city-dropdown', 'value')]\n",
@@ -78,19 +84,14 @@
     "\n",
     "    return fig\n",
     "\n",
-    "# 起動部分\n",
     "if __name__ == \"__main__\":\n",
-    "    in_jupyter = \"JPY_PARENT_PID\" in os.environ or \"ipykernel_launcher\" in os.sys.argv[0]\n",
-    "    if in_jupyter:\n",
-    "        app.run_server(debug=True, port=8050)  # Jupyter用\n",
-    "    else:\n",
-    "        port = int(os.environ.get(\"PORT\", 10000))\n",
-    "        app.run(host=\"0.0.0.0\", port=port, debug=False)  # Renderなど外部サーバー用\n"
+    "    port = int(os.environ.get(\"PORT\", 10000))\n",
+    "    app.run(host=\"0.0.0.0\", port=port, debug=False)\n"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": None,
+   "execution_count": null,
    "id": "f7bfb9a0-b1cf-48d2-84d4-26235ba6aaae",
    "metadata": {},
    "outputs": [],
